1a510b4ce7196abf56270c59f228310135390513
f47iw4 <fgrhanny@gmail.com>
2025-02-13 20:25:32 +0100

    Add especialidades(), medicos() to routes

--- clinica/routes/web.php
+++ clinica/routes/web.php
@@ -11,12 +11,18 @@ Route::get('/', function () {
 
 Route::resource('medicos', MedicoController::class);
 Route::resource('especialidades', EspecialidadController::class);
-/* ruta para mostrar especialidades */
-/* Route::get('/especialidades/{id}', [EspecialidadController::class, 'show'])->name('especialidades.show');
- *//* para el index de medicos */
-/* Route::get('/medicos', [MedicoController::class, 'index'])->name('medicos.index');
- *//* para mostrar más detalles de ese index medicos */
-/* Route::get('/medicos/{id}', [MedicoController::class, 'show'])->name('medicos.show');
-/*Route::get('/especialidades', [EspecialidadController::class, 'index'])->name('especialidades.index');
- */
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/especialidades', function () {
+    return view('especialidades.index');
+});
+
+Route::get('/medicos', function () {
+    return view('medicos.index');
+});
+
 Route::get('/especialidades/{id}/medicos', [EspecialidadController::class, 'medicos'])->name('especialidades.medicos');
