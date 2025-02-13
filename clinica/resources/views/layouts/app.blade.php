dbe0ef0a5f0268a6efb19cdeb1f637074efec958
irenepp <ire.pachpan@gmail.com>
2025-02-13 20:09:23 +0100

    actualizacion layouts.app para arreglar el bootstrap de mis vistas

--- clinica/resources/views/layouts/app.blade.php
+++ clinica/resources/views/layouts/app.blade.php
@@ -3,11 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
     <title>@yield('title', 'Mi Aplicación')</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
-    <!-- este header se puede cambiar, decir a Fati -->
     <header>
       <!-- Navbar -->
       <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
@@ -37,6 +37,6 @@
     <footer>
         <p>© 2025 ClínicaOn. Todos los derechos reservados.</p>
     </footer>
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
