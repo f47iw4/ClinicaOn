70510231f3281f60693e31680d199caef30b52d3
f47iw4 <fgrhanny@gmail.com>
2025-02-15 18:41:31 +0100

    Add fixed-bottom class to footer

--- clinica/app/Http/Controllers/SearchController.php
+++ clinica/app/Http/Controllers/SearchController.php
@@ -11,16 +11,17 @@ class SearchController extends Controller
     // Método que maneja la búsqueda
     public function search(Request $request)
     {
-        // Obtener el término de búsqueda desde el formulario
+        // Obtener la búsqueda desde el formulario
         $query = $request->input('query');
 
-        // Realizar la búsqueda en el modelo de Especialidades
-        $especialidades = Especialidad::where('nombre', 'like', "%$query%")->get();
+        // Realizar la búsqueda en el modelo de Especialidades con paginación (paginate en lugar de get)
+        $especialidades = Especialidad::where('nombre', 'like', "%$query%")->paginate(10);
 
-        // Realizar la búsqueda en el modelo de Médicos
-        $medicos = Medico::where('nombre', 'like', "%$query%")->get();
+        // Realizar la búsqueda en el modelo de Médicos con paginación
+        $medicos = Medico::where('nombre', 'like', "%$query%")->paginate(10);
 
         // Retornar los resultados a la vista de búsqueda
         return view('search.results', compact('especialidades', 'medicos', 'query'));
     }
+    
 }
--- clinica/resources/views/layouts/app.blade.php
+++ clinica/resources/views/layouts/app.blade.php
@@ -60,12 +60,11 @@
     <main>
         @yield('content')
     </main>
-
-    <footer class="bg-primary text-white py-3">
-    <div class="container text-center">
-        <p>© 2025 Enfermitos. Todos los derechos reservados.</p>
-    </div>
-</footer>
+    <footer class="bg-primary text-white py-3 fixed-bottom">
+        <div class="container text-center">
+            <p>© 2025 Enfermitos. Todos los derechos reservados.</p>
+        </div>
+    </footer>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
